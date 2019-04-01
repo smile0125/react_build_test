0508e0f20fcb50d7c5b20cf2820a7ff8a155e6ad
juguohong <1045416962@qq.com>
2019-04-01 17:42:58 +0800

    1

---
+++ precache-manifest.0f312701f4628c5348c645dabbb499c4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f61c76edb850b7c5e18ea541d242fe7f",
+    "url": "/static/media/user_banner_bg.f61c76ed.png"
+  },
+  {
+    "revision": "c7e0158424ed6d32b4b1",
+    "url": "/static/css/main.f0ee42b7.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dbe9582764baefd1d73d",
+    "url": "/static/js/2.dbe95827.chunk.js"
+  },
+  {
+    "revision": "ed4e832a150638811ad21b4adb04c807",
+    "url": "/static/media/tuijian_item1.ed4e832a.png"
+  },
+  {
+    "revision": "107bccbed395b440395904307073937c",
+    "url": "/static/media/cipian_1.107bccbe.png"
+  },
+  {
+    "revision": "c7e0158424ed6d32b4b1",
+    "url": "/static/js/main.c7e01584.chunk.js"
+  },
+  {
+    "revision": "5d2e3ed068c6b5c758971f1cf1cca625",
+    "url": "/static/media/banner_bg.5d2e3ed0.png"
+  },
+  {
+    "revision": "665231ddc84bfc42493850626638a654",
+    "url": "/static/media/banner.665231dd.png"
+  },
+  {
+    "revision": "356bb751b38407c6722f27ff2ffe1fed",
+    "url": "/static/media/go.356bb751.png"
+  },
+  {
+    "revision": "d9f70c2528b49619c81beb1ee33eade5",
+    "url": "/static/media/cipian_2.d9f70c25.png"
+  },
+  {
+    "revision": "932ac4fedb3a857446fd08217baf289b",
+    "url": "/static/media/cipian_3.932ac4fe.png"
+  },
+  {
+    "revision": "d8741b3a2a30bed51b9b321d656cd385",
+    "url": "/index.html"
+  }
+];
